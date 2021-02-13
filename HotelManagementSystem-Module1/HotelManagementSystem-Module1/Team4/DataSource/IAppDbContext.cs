954111dc803f2767c52673e058600eb9c9010614
derjr96 <54965311+derjr96@users.noreply.github.com>
2021-02-13 20:46:40 +0800

    Base Model Class

---
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Team4/DataSource/IAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HotelManagementSystem_Module1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem_Module1.Team4.DataSource
+{
+    public interface IAppDbContext
+    {
+        public DbSet<Reservation> ReservationsDb();
+    }
+}

ad94085dc8a15b81cb9666ca8684b4ef221c6d25
derjr96 <54965311+derjr96@users.noreply.github.com>
2021-02-16 22:39:19 +0800

    Created Reservation page (To be Updated)

---
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/CreateReservationController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem_Module1.Presentation.Controllers
+{
+    public class CreateReservationController
+    {
+    }
+}

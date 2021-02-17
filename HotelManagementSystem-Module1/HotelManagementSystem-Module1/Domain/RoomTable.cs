12b32a21b1d761761a4c27b1b04e6e34f75c9d8d
WanLoong <55482208+WanLoong@users.noreply.github.com>
2021-02-17 19:52:29 +0800

    test

--- HotelManagementSystem-Module1/HotelManagementSystem-Module1/Domain/RoomTable.cs
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Domain/RoomTable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelManagementSystem_Module1.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
--- HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/RoomController.cs
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/RoomController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelManagementSystem_Module1.Models;
+using HotelManagementSystem_Module1.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +11,17 @@ namespace HotelManagementSystem_Module1.Presentation.Controllers
 {
     public class RoomController : Controller
     {
-        public IActionResult Index()
+        public IActionResult ViewAvailability()
         {
+            IRoom roomTable = new RoomTable();
+            Room room = new Room();
+            
             return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }

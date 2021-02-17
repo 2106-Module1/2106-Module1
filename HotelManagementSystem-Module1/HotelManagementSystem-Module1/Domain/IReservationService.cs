efbb2a2d76179363e53a5f937e125a1b6ea3477d
derjr96 <54965311+derjr96@users.noreply.github.com>
2021-02-17 20:41:22 +0800

    Created ReservationService

---
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Domain/IReservationService.cs
@@ -0,0 +1,23 @@
+﻿using HotelManagementSystem_Module1.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem_Module1.Domain
+{
+    public interface IReservationService
+    {
+        IEnumerable<Reservation> GetAllReservations();
+
+        IEnumerable<Reservation> SearchByReservationId(int id);
+
+        IEnumerable<Reservation> SearchByGuestId(int id);
+
+        bool CreateReservation(Reservation reservation);
+
+        bool DeleteReservation(int id);
+
+        public bool UpdateReservation(Reservation reservation);
+    }
+}

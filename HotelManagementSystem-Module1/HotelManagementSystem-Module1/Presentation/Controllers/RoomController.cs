fc9ca275ed1f1e1c7a8dd05a4b0fd5e5c10d1eeb
WanLoong <55482208+WanLoong@users.noreply.github.com>
2021-02-17 20:35:03 +0800

    Implement View

--- HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/RoomController.cs
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/RoomController.cs
@@ -19,9 +19,11 @@ namespace HotelManagementSystem_Module1.Presentation.Controllers
             Room room1 = new Room(1, 101, "twin", 25, 1, "a", false);
             Room room2 = new Room(1, 101, "single", 25, 1, "b", false);
             List<Room> lst = new List<Room>();
-            lst.Add(room);
-            lst.Add(room1);
-            lst.Add(room2);
+            IRoom roomTable = new RoomTable();
+            //roomTable.CreateRoom(room);
+            //lst.add(room);
+            //lst.add(room1);
+            //lst.add(room2);
             ViewBag.lst = lst;
             return View();
         }

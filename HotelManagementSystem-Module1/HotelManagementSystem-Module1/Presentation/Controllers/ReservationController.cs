91b5032c04b8ffa8e6e62763aae4be0f8fda108e
derjr96 <54965311+derjr96@users.noreply.github.com>
2021-02-19 14:51:08 +0800

    Organised and Commented all Implemented Codes

--- HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/ReservationController.cs
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/ReservationController.cs
@@ -12,6 +12,7 @@ using System.Collections;
 
 /*
  * Owner of ReservationController: Mod 1 Team 4
+ * This Controller is used for Viewing Records only.
  */
 namespace HotelManagementSystem_Module1.Presentation.Controllers
 {
@@ -26,6 +27,13 @@ namespace HotelManagementSystem_Module1.Presentation.Controllers
             _reservationService = reservationService;
 
         }
+
+        /*
+         * <summary>
+         * (Completed)
+         * Function to retrieve all existing reservations
+         * </summary>
+         */
         public IActionResult ReservationView()
         {
             // Initialising Variables
@@ -65,77 +73,30 @@ namespace HotelManagementSystem_Module1.Presentation.Controllers
             ViewBag.mainList = mainList;
             return View();
         }
-        
-        [HttpGet]
-        public IActionResult CreateReservation()
+
+        /*
+         * <summary>
+         * Function to retrieve all the reservations for a particular guest
+         * TODO: for Deliverable 3
+         * </summary>
+         */
+        public IActionResult ViewAllGuestRecord()
         {
-            // Initialising Variables 
-            Dictionary<string, object> resTemp = new Dictionary<string, object>();
-            Dictionary<string, int> guestDetail = new Dictionary<string, int>();
-            List<string> guestName = new List<string>();
-            string[] resFields = { "Number of Guests", "Room Type", "Check-In Date/Time", "Check-Out Date/Time", "Remarks", "Promotion Code", "Price" };
-            
-            // Retrieve all existing guests based on Mod 1 Team 9 function
-            IEnumerable<Guest> guestList = _guestService.RetrieveGuests();
-
-            // For loop to store existing guest to populate View Form DropDownList
-            foreach (var guest in guestList)
-            {
-                guestName.Add(guest.FirstNameDetails() + " " + guest.LastNameDetails());
-                guestDetail.Add((guest.FirstNameDetails() + " "+guest.LastNameDetails()),guest.GuestIdDetails());
-            }
-
-            // storing view Form data type to show on View Form
-            resTemp.Add("Guest Name",default(string));
-            resTemp.Add("Number of Guests", default(int));
-            resTemp.Add("Room Type", default(string));
-            resTemp.Add("Check-In Date/Time", DateTime.Now.Date.AddHours(10));
-            resTemp.Add("Check-Out Date/Time", DateTime.Now.Date.AddHours(14));
-            resTemp.Add("Remarks", default(string));
-            resTemp.Add("Promotion Code", default(string));
-            resTemp.Add("Price", default(double));
-
-            // Passing data over to View Page via ViewBag "/Reservation/CreateReservation"
-            ViewBag.guestName = guestName;
-            ViewBag.reservationTemp = resTemp;
-            ViewBag.guestDetail = guestDetail;
-
-            return View(resTemp);
+            //TODO : retrieve all the reservations for a particular guest
+            throw new NotImplementedException();
         }
 
-        [HttpPost]
-        public IActionResult CreateReservation(Dictionary<string, object> newReservation)
+        /*
+         * <summary>
+         * Function to retrieve a single Reservation Record to display in detail.
+         * This function will link to Update Reservation if there is a need to update.
+         * TODO: for Deliverable 3
+         * </summary>
+         */
+        public IActionResult ViewReservationRecord()
         {
-            // Initialising Variables
-            IEnumerable<Guest> guestList = _guestService.RetrieveGuests();
-            Dictionary<string, int> guestDetail = new Dictionary<string, int>();
-            
-            foreach (var guest in guestList)
-            {
-                guestDetail.Add((guest.FirstNameDetails() + " " + guest.LastNameDetails()), guest.GuestIdDetails());
-            }
-
-            Dictionary<string, object> resTemp = new Dictionary<string, object>();
-
-            // Add all POST data into a dictionary
-            int guestID = guestDetail[Request.Form["Guest Name"]];
-            resTemp.Add("guestID", guestID);
-            resTemp.Add("numOfGuest", Convert.ToInt32(Request.Form["Number of Guests"].ToString()));
-            resTemp.Add("roomType", Request.Form["Room Type"].ToString());
-            resTemp.Add("start", Convert.ToDateTime(Request.Form["Check-In Date/Time"].ToString()));
-            resTemp.Add("end", Convert.ToDateTime(Request.Form["Check-Out Date/Time"].ToString()));
-            resTemp.Add("remark", Request.Form["Remarks"].ToString());
-            resTemp.Add("modified", DateTime.Now);
-            resTemp.Add("promoCode", Request.Form["Promotion Code"].ToString());
-            resTemp.Add("price", Convert.ToDouble(Request.Form["Price"].ToString()));
-            resTemp.Add("status", "Not Fulfilled");
-            
-            // Creating Reservation object and storing it to database
-            Reservation createdReservation = (Reservation)new Reservation().SetReservation(resTemp);
-            _reservationService.CreateReservation(createdReservation);
-
-            // After completion of creation to redirect user to "/Reservation/ReservationView"
-            return RedirectToAction("ReservationView");
+            //TODO : check database for clashing reservation timings
+            throw new NotImplementedException();
         }
 
     }
---
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/ReservationTrendController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelManagementSystem_Module1.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+/*
+ * Owner of ReservationManagementController: Mod 1 Team 4
+ * This Controller is used for Manipulated and Analysis All Reservations
+ * to create a Cancellation trend and Reservation Forecast.
+ */
+namespace HotelManagementSystem_Module1.Presentation.Controllers
+{
+    public class ReservationTrendController : Controller
+    {
+        private readonly IReservationService _reservationService;
+        private readonly IGuestService _guestService;
+
+        public ReservationTrendController(IReservationService reservationService, IGuestService guestService)
+        {
+            _guestService = guestService;
+            _reservationService = reservationService;
+        }
+
+        /*
+         * <summary>
+         * Analysis Function to view Cancellation Trends
+         * TODO for Deliverable 3
+         * </summary>
+         */
+        public IActionResult CancellationTrend()
+        {
+            //TODO : Create a line graph to show how many cancellations by date to see a trending
+            throw new NotImplementedException();
+        }
+
+        /*
+         * <summary>
+         * Analysis Function to view Check in Forecast Trends
+         * This is to better prepare Managers to allocate staffing properly during high check in forecast
+         * TODO for Deliverable 3
+         * </summary>
+         */
+        public IActionResult CheckInTrend()
+        {
+            //TODO : Create a line graph to show how many check in reservations by date
+            throw new NotImplementedException();
+        }
+    }
+}

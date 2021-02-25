26fafc4f5a0b7d187ef3d738fe486da54facd73e
JohnLeong <johnleongjs@gmail.com>
2021-02-25 17:35:12 +0800

    Mod 3 public area API

---
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Domain/IPublicArea.cs
@@ -0,0 +1,13 @@
+﻿using HotelManagementSystem_Module1.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem_Module1.Domain
+{
+    public interface IPublicArea
+    {
+        List<PublicAreaDTO> getAllFacilityResults();
+    }
+}

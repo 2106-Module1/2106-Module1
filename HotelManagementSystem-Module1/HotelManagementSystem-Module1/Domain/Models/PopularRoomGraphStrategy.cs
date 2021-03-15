62e3cd8f3bfe757617f6b6e39b31f4e37493a65d
puglord96 <55310319+puglord96@users.noreply.github.com>
2021-03-15 23:31:50 +0800

    Implemented Strategy Design Pattern for Reservation Trend Controller

---
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Domain/Models/PopularRoomGraphStrategy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem.Domain.Models
+{
+    public class PopularRoomGraphStrategy : IAnalyticsStrategy
+    {
+        
+
+        int[] IAnalyticsStrategy.GenerateChartValues(IEnumerable<Reservation> ReservationList)
+        {
+            int[] popularRoomTypeArr = { 0, 0, 0, 0 };
+
+            foreach (var res in ReservationList)
+            {
+                string roomType = (string)res.GetReservation()["roomType"];
+
+                switch (roomType)
+                {
+                    case "Double":
+                        int doubleRoom = popularRoomTypeArr[0] + 1;
+                        popularRoomTypeArr[0] = doubleRoom;
+                        break;
+                    case "Twin":
+                        int TwinRoom = popularRoomTypeArr[1] + 1;
+                        popularRoomTypeArr[1] = TwinRoom;
+                        break;
+                    case "Family":
+                        int FamilyRoom = popularRoomTypeArr[2] + 1;
+                        popularRoomTypeArr[2] = FamilyRoom;
+                        break;
+                    case "Suite":
+                        int SuiteRoom = popularRoomTypeArr[3] + 1;
+                        popularRoomTypeArr[3] = SuiteRoom;
+                        break;
+                    default:
+                        throw new NullReferenceException("Room Type cannot be found");
+                }
+            }
+
+            return popularRoomTypeArr;
+        }
+
+        string[] IAnalyticsStrategy.GenerateChartXAxis()
+        {
+            String[] roomTypes = { "Double", "Twin", "Family", "Suite" };
+            return roomTypes;
+        }
+    }
+}
--- HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/ReservationTrendController.cs
+++ HotelManagementSystem-Module1/HotelManagementSystem-Module1/Presentation/Controllers/ReservationTrendController.cs
@@ -34,134 +34,36 @@ namespace HotelManagementSystem.Presentation.Controllers
             //TODO : Create a line graph to show how many cancellations by date to see a trending
             DateTime todayDate = DateTime.Now;
 
-            //--------------------------- CANCELLATION GRAPH ----------------------------------------------------------------
+ 
 
-            //For creating the x-axis labels for Cancellation Graph-----------------------------------------------------
-
-            ArrayList xAxisMonthYear = new ArrayList();
-
-            for (int i = 11; i > 0; i--)
-            {
-
-                DateTime insertDate = DateTime.Now.AddMonths(-i);
-
-                String formattedXAxisString = insertDate.ToString("MMM") + "-" + insertDate.ToString("yy");
-                xAxisMonthYear.Add(formattedXAxisString);
-
-            }
-            xAxisMonthYear.Add(todayDate.ToString("MMM") + "-" + todayDate.ToString("yy"));
-
-            String[] xAxisMonthYearArr = (String[])xAxisMonthYear.ToArray(typeof(string));
-            ViewBag.xAxisMonthYearArr = xAxisMonthYearArr;
-
-            //-------------------------------------------------------------------------------------------------------------
-
-            //Generated Data for Cancelled Reservations (Both "Cancelled" and "Not Fulfilled(No Show)"-------------------
-
+            AnalyticsContext context = new AnalyticsContext();
+            context.setAnalyticsStrategy(new CancellationTrendStrategy());
             IEnumerable<Reservation> reservationCancelledList = _reservationService.GetReservationStatusByDate("Cancelled", todayDate.AddMonths(-11), todayDate);
-
-
-            int[] xAxisDataArr = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-            foreach (var test in reservationCancelledList)
-
-            {
-                DateTime startDate = (DateTime)test.GetReservation()["modified"];
-
-                int monthDifference = ((todayDate.Year - startDate.Year) * 12) + todayDate.Month - startDate.Month;
-
-                int xAxisPosition = 11 - monthDifference;
-
-                int v = xAxisDataArr[xAxisPosition] + 1;
-                xAxisDataArr[xAxisPosition] = v;
-            }
-
-            ViewBag.cancellationGraphData = xAxisDataArr;
-            //---------------------------------------------------------------------------------------------
-
-            //-------   CHECK-IN GRAPH------------------------------------------------------------------------
-
-            //------------ Generating X-Axis for Check In Graph ----------------------------------------------------------------
-            ArrayList XAxisCheckIn = new ArrayList();
-
-            for (int i = 31; i >= 0; i--)
-            {
-
-                DateTime insertDate = DateTime.Now.AddDays(-i);
-
-                String formattedXAxisString = insertDate.ToString("dd") + " - " + insertDate.ToString("MMM") + "-" + insertDate.ToString("yy");
-                XAxisCheckIn.Add(formattedXAxisString);
-            }
-
-            String[] XAxisCheckInArr = (String[])XAxisCheckIn.ToArray(typeof(string));
-            ViewBag.XAxisCheckInArr = XAxisCheckInArr;
-
-            //------------------------------------------------------------------------------------------------------
-
-            //Generated Data for Check-in Numbers (Reservation Fulfilled)----------------------------------------------------------------------
             IEnumerable<Reservation> checkedInList = _reservationService.GetReservationStatusByDate("Not Fulfilled", todayDate.AddDays(-30), todayDate);
-
-            int[] checkInArr = new int[31];
-
-            foreach (var res in checkedInList)
-            {
-                DateTime startDate = (DateTime)res.GetReservation()["start"];
-
-                int dateDifference = (int)(todayDate - startDate).TotalDays;
-
-                int checkInXAxisPosition = 30 - dateDifference;
-
-                int v = checkInArr[checkInXAxisPosition] + 1;
-                checkInArr[checkInXAxisPosition] = v;
-            }
-
-            ViewBag.checkInGraphData = checkInArr;
-
-            //--------------------------------------------------------------------------------------------------------
-
-            //--------------POPULAR ROOM GRAPH-------------------------------------------------
-
-            //Generating X-Axis for Popular Room Graphs--------------------------------------
-            String[] roomTypes = { "Double", "Twin", "Family", "Suite" };
-
-            ViewBag.xAxisPopularRoomArr = roomTypes;
-
-            //-----------------------------------------------------------------------------
-
-            //Generated Data for Popular Rooms---------------------------------------------------------------------
             IEnumerable<Reservation> allReservations = _reservationService.GetAllReservations();
 
-            int[] popularRoomTypeArr = { 0, 0, 0, 0 };
+            String[] xAxisMonthYearArr = context.GenerateAnalyticsChartXAxis();
+            int[] xAxisDataArr = context.GenerateAnalyticsChartValues(reservationCancelledList);
 
-            foreach (var res in allReservations)
-            {
-                string roomType = (string)res.GetReservation()["roomType"];
+            
+            ViewBag.xAxisMonthYearArr = xAxisMonthYearArr;
+            ViewBag.cancellationGraphData = xAxisDataArr;
 
-                switch (roomType)
-                {
-                    case "Double":
-                        int doubleRoom = popularRoomTypeArr[0] + 1;
-                        popularRoomTypeArr[0] = doubleRoom;
-                        break;
-                    case "Twin":
-                        int TwinRoom = popularRoomTypeArr[1] + 1;
-                        popularRoomTypeArr[1] = TwinRoom;
-                        break;
-                    case "Family":
-                        int FamilyRoom = popularRoomTypeArr[2] + 1;
-                        popularRoomTypeArr[2] = FamilyRoom;
-                        break;
-                    case "Suite":
-                        int SuiteRoom = popularRoomTypeArr[3] + 1;
-                        popularRoomTypeArr[3] = SuiteRoom;
-                        break;
-                    default:
-                        throw new NullReferenceException("Room Type cannot be found");
-                }
-            }
 
+            context.setAnalyticsStrategy(new CheckInTrendStrategy());
+
+            String[] XAxisCheckInArr = context.GenerateAnalyticsChartXAxis();
+            int[] checkInArr = context.GenerateAnalyticsChartValues(checkedInList);
+            ViewBag.XAxisCheckInArr = XAxisCheckInArr;
+            ViewBag.checkInGraphData = checkInArr;
+
+            context.setAnalyticsStrategy(new PopularRoomGraphStrategy());
+            String[] xAxisPopularRoomArr = context.GenerateAnalyticsChartXAxis();
+            int[] popularRoomTypeArr = context.GenerateAnalyticsChartValues(allReservations);
+
+            ViewBag.xAxisPopularRoomArr = xAxisPopularRoomArr;
             ViewBag.popularRoomGraphData = popularRoomTypeArr;
-            //------------------------------------------------------------------------------------------------------- 
+
 
             return View();
         }
